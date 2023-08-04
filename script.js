f2c41c3fb530f1ffcf00bacc8a3b2628207e8741
felipegeneroso <felipegenerosobr@gmail.com>
2023-08-04 11:27:56 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
 const form = document.querySelector('#form');
 
-form.addEventListener('submit', function(e){
-e.preventDefault();
-console.log("previnido")
-setResultado("ola mundo")
-})
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
 
-function setResultado (msg){
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso Inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura Inválido', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado (msg, true);
+});
+
+function getNivelImc(imc) {
+    const nivel = ['abixo do peso', 'Peso normal', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function criaP() {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(msg, isValid) {
     const resultado = document.querySelector("#resultado");
-    resultado.innerHTML = msg;
+    resultado.innerHTML = '';
+    const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+    p.innerHTML = msg
+    resultado.appendChild(p);
 }
